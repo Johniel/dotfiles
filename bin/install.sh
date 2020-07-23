bdf79ece6cc9b378457115ffeabfc2938040f18f
Johniel <johniel.s.m@gmail.com>
2020-07-23 12:52:52 +0900

    added .gitignore

---
+++ bin/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ue
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install bat
+brew install exa
+brew install fd
+brew install fish
+brew install rd
