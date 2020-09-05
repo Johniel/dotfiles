fd7ffd4e865e9193878a770d752e4697d05dc0bd
Johniel <johniel.s.m@gmail.com>
2020-09-06 05:58:27 +0900

    use git-delta

--- bin/install.sh
+++ bin/install.sh
@@ -7,11 +7,24 @@ brew install exa
 brew install fd
 brew install fish
 brew install rd
+brew install git-delta
 
 git clone git@github.com:Johniel/.emacs.d.git
 git clone git@github.com:Johniel/contests.git
-mkdir projects
-mkdir workspace
+mkdir ~/projects
+mkdir ~/workspace
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fisher add jethrokuan/z
+
+cat <<EOS >> ~/.gitconfig
+[core]
+    pager = delta --plus-color="#012800" --minus-color="#340001" --theme='Monokai Extended'
+
+[interactive]
+    diffFilter = delta --color-only
+
+[delta]
+    side-by-side = true
+    line-numbers = true
+EOS
